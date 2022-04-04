7240ac96ae58cfecb3423b4d61014dbfc5b199b1
dbtop <berdaddario@gmail.com>
2022-04-04 11:13:40 -0300

    fix: itemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -2,14 +2,14 @@ import { useState,useEffect } from 'react'
 
 const ItemCount = ({ stock, initial, onAdd })=> {
     
-    const [count, setCount] = useState(1)
+    const [count, setCount] = useState(0)
     
     useEffect(() => {
         console.log("componente listo")
     }, [count])
        
-    const increase = ()=>{ count < stock ? setCount(count + 1 ) : setCount(count)  }
-    const decrease = ()=>{ (count <= stock && count > 1) ? setCount(count - 1 ) : setCount(count)  }
+    const increase = ()=>{ count < stock ? setCount(count + 1 ) : null  }
+    const decrease = ()=>{ (count <= stock && count > 0) ? setCount(count - 1 ) : null  }
 
  
 return(
